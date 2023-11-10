4306d9e5d7c35b4ce5234dba3f8a440d68468e37
elizashanker <elizashanker@gmail.com>
2023-11-10 13:55:52 +0300

    доделала))

---
+++ program/run.py
@@ -0,0 +1,9 @@
+from main import *
+import sys
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    app.setStyle('Fusion')
+    ex = MainWindow()
+    ex.show()
+    sys.exit(app.exec())
--- run.py
+++
@@ -1,9 +0,0 @@
-from main import *
-import sys
-
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    app.setStyle('Fusion')
-    ex = MainWindow()
-    ex.show()
-    sys.exit(app.exec())
